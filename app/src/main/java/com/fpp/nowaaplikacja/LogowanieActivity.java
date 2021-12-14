355b30359dab09ca9a950319d884aceb72934217
Filippo0420 <fpawelec0@gmail.com>
2021-12-14 15:07:03 +0100

    gcommit

---
+++ app/src/main/java/com/fpp/nowaaplikacja/LogowanieActivity.java
@@ -0,0 +1,14 @@
+package com.fpp.nowaaplikacja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogowanieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logowanie);
+    }
+}
--- app/src/main/java/com/fpp/nowaaplikacja/MainActivity.java
+++ app/src/main/java/com/fpp/nowaaplikacja/MainActivity.java
@@ -1,14 +1,23 @@
 package com.fpp.nowaaplikacja;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar myToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        myToolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle("Toolbar");
     }
 }
